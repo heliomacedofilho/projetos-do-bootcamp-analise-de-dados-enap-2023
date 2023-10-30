fdbd45ce399f0c95b6ece24043de9f7539af92fc
dridrileite <89482697+dridrileite@users.noreply.github.com>
2023-10-30 16:27:00 -0300

    Atualização app_patentes

--- mapeamento_das_patentes_depositadas_pelas_ifes/app_patentes.py
+++ mapeamento_das_patentes_depositadas_pelas_ifes/app_patentes.py
@@ -12,12 +12,14 @@ import numpy as np
 
 
 st.set_page_config(page_title="Patentes Depositadas pelas Intituições Federais de Ensino", layout="wide")
-with st.container():
-    st.markdown('# Mapeamento das IFES e Institutos Federais')
-    st.markdown("---")
-    st.sidebar.title('Áreas Tecnológicas')
-    st.sidebar.selectbox('Área Tecnologica',('Nacional','Por Região e Estado'))
-    
+
+st.markdown('# Mapeamento das IFES e Institutos Federais')
+st.markdown("---")
+st.sidebar.title('Áreas Tecnológicas')
+#st.sidebar.selectbox('Área Tecnologica',('Nacional','Por Região e Estado'))
+
+aba1, aba2 = st.tabs(['Nacional', 'Região e Estado'])
+
     
 
 @st.cache_data    
@@ -27,13 +29,24 @@ def carregar_dados():
     dados=pd.read_csv("area_tecnologica.csv")
     return dados
 
-        
-with st.container():
-    
-    dados_tratado=carregar_dados()
-    #grouped=dados.groupby('Area').agg(contagem=('Area','count')).sort_values(by='contagem',ascending=False)
-    
-    #opcoes=st.selectbox("Selecione a Opção",["Todas Instituições", "Universidades","Institutos Federais"])
+dados_tratado=carregar_dados()
+
+data_analise_IF = dados_tratado[
+    dados_tratado['titular_1'].str.contains('^INSTITUTO|^CENTRO FEDERAL') |
+    dados_tratado['titular_parceiros1'].str.contains('^INSTITUTO|^CENTRO FEDERAL') |
+    dados_tratado['titular_parceiros2'].str.contains('^INSTITUTO|^CENTRO FEDERAL')
+]
+
+data_analise_UV = dados_tratado[
+    dados_tratado['titular_1'].str.contains('UNIVER') |
+    dados_tratado['titular_parceiros1'].str.contains('^UNIVER') |
+    dados_tratado['titular_parceiros2'].str.contains('^UNIVER')
+]
+data_analise_total=pd.concat([data_analise_IF, data_analise_UV])
+
+with aba1:
+       
+   
     def plot_graph(grouped):
        
         
@@ -59,29 +72,21 @@ with st.container():
 
     option = st.selectbox(
     'Escolha o grupo de dados:',
-    ('Todas Instituições','Institutos e Centros Federais', 'Universidades'))
+    ('Todas Instituições','Institutos e Centros Federais', 'Universidades'),key='unique_key_1' )
 
     # Filtragem de dados baseada na seleção
     if option == 'Todas Instituições':
-        data_analise = dados_tratado
+        data_analise = data_analise_total
         pallete="Wistia"
         title = "Área Tecnológica: Todas Instituições"
         
     elif option == 'Institutos e Centros Federais':
-        data_analise = dados_tratado[
-            dados_tratado['titular_1'].str.contains('^INSTITUTO|^CENTRO FEDERAL') |
-            dados_tratado['titular_parceiros1'].str.contains('^INSTITUTO|^CENTRO FEDERAL') |
-            dados_tratado['titular_parceiros2'].str.contains('^INSTITUTO|^CENTRO FEDERAL')
-        ]
-        pallete="YlGn"
+        data_analise =data_analise_IF
+        pallete="YlGn_d"
         title = "Área Tecnológica: Institutos e Centros Federais"
     elif option == 'Universidades':
-        data_analise = dados_tratado[
-            dados_tratado['titular_1'].str.contains('UNIVER') |
-            dados_tratado['titular_parceiros1'].str.contains('^UNIVER') |
-            dados_tratado['titular_parceiros2'].str.contains('^UNIVER')
-        ]
-        pallete="YlGnBu"
+        data_analise = data_analise_UV
+        pallete="YlGnBu_d"
         title = "Área Tecnológica: Universidades"
         
 
@@ -90,5 +95,38 @@ with st.container():
 
     # Mostre o gráfico
     plot_graph(grouped_data)
+
+with aba2:
+    
+    
+    def generate_treemap(data):
+        grouped_regiao = data.groupby(['Regiao', 'titular_uf1', 'Area']).agg(contagem=('Area', 'count'))
+        grouped_regiao_sorted = grouped_regiao.sort_values(by=['titular_uf1', 'contagem'], ascending=[True, False])
+        top_5_per_state = grouped_regiao_sorted.groupby('titular_uf1').head(5).reset_index()
+
+        fig = px.treemap(top_5_per_state, 
+                         path=[px.Constant("Brasil"),'Regiao', 'titular_uf1', 'Area'], 
+                         values='contagem', 
+                         color='Area',
+                         color_continuous_scale='RdBu')
+        fig.update_layout(margin=dict(t=50, l=25, r=25, b=25))
+
+        st.plotly_chart(fig)
+
+          
+    option = st.selectbox(
+    'Escolha o grupo de dados:',
+    ('Todas Instituições','Institutos e Centros Federais', 'Universidades'),key='unique_key_2' )
+    
+    if option == 'Todas Instituições':
+        generate_treemap(data_analise_total)
+    elif option == 'Institutos e Centros Federais':
+        generate_treemap(data_analise_IF)
+    elif option == 'Universidades':
+        generate_treemap(data_analise_UV)
+
+
+
+    
 
 
