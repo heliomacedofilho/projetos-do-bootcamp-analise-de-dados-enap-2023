b06735934443bded75ca4c3b10ec5fd02b49b406
Alessandra Menezes <AASMenezes@users.noreply.github.com>
2023-10-31 10:41:48 -0300

    Atualiza visualização - app
    Atualiza visualização - app

--- mapeamento_das_patentes_depositadas_pelas_ifes/app_ales.py
+++ mapeamento_das_patentes_depositadas_pelas_ifes/app_ales.py
@@ -6,10 +6,10 @@ import seaborn as sns
 from plotly.offline import iplot
 pd.options.display.float_format = '{:.1f}'.format
 
-st.set_page_config(page_title="Período de depósitos das patentes: 01/2018 a 01/2023", layout="wide")
-st.title('Patentes depositadas entre 01/2018 e 01/2023')
+st.set_page_config(layout="wide")
+st.markdown("### Patentes depositadas entre Jan/2018 e Jan/2023")
 
-df = pd.read_excel('dados_patentes_longo_AM.xlsx')
+df = pd.read_csv('dados_patentes_longo_AM.csv')
 df['data_deposito'] = pd.to_datetime(df['data_deposito'], format="%d/%m/%Y")
 df['ifes'] = df['ifes'].astype('category')
 df['area'] = df['area'].astype('category')
@@ -25,7 +25,7 @@ df['ano_dep'] = df['ano_dep'].astype('int')
 df['mes_dep'] = df['mes_dep'].astype('int')
 df['mes_ano_dep'] = df['mes_ano_dep'].astype('str')
 
-st.sidebar.title('Localização Geográfica do Titular da Patente')
+st.sidebar.markdown('#### Localização Geográfica do Titular da Patente')
 filtro = st.sidebar.radio('Selecione uma opção' ,
                           ['Tudo', 'Brasil', 'Norte do Brasil', 'Nordeste do Brasil',
                            'Centro-Oeste do Brasil', 'Sudeste do Brasil', 'Sul do Brasil'])
@@ -55,77 +55,97 @@ UV = df[(df['ifes'] == 'Sim') & (df['responsavel'].str.contains('^UNIVER'))].cop
 IFE = df[df['ifes'] == 'Sim'].copy()
 NIFE = df[df['ifes'] == 'Não'].copy()
 
-total_geral = df[df['data_deposito'] >= '2018-01-01']['ID'].nunique()
-total_ifes = df.loc[(df['ifes'] == 'Sim') & (df['data_deposito'] >= '2018-01-01')]['ID'].nunique()
-total_nifes = NIFE[(~NIFE['ID'].isin(IFE['ID'])) & (NIFE['data_deposito'] >= '2018-01-01')]['ID'].nunique()
-    
-st.metric('Quantidade de Patentes (geral)', total_geral)
-st.metric('Quantidade de Patentes com participação de IFEs', total_ifes)
-st.metric('Quantidade de Patentes sem paricipação de IFEs', total_nifes)
-
 aba1, aba2, aba3, aba4 = st.tabs(['Todas instituições Federais de Ensino',
                                   'Institutos e Centros Federais',
                                   'Universidades Federais',
                                   'Outras instituições, empresas e organizações'])
 
 
-def plot_graph(counts):
-    fig, ax = plt.subplots(figsize=(8, 6))
-    sns.lineplot(x='mes_ano_dep', y='ID', data=counts)
-    plt.xticks(rotation=90)  # Rotacionar os rótulos no eixo x para melhor legibilidade
-    plt.xlabel('\nData de depósito da patente')
-    plt.ylabel('Número absoluto de patentes depositadas\n')
-    #plt.subplots_adjust(top=0.9,right = 0.9)
-    # Ajuste a figura
-    plt.tight_layout()
-    # Mostre a figura no Streamlit
-    st.pyplot(fig)
-
-def plot_graph(counts):
-    fig = px.line(counts,
-                  x='mes_ano_dep', y='ID',
-                  markers=True,
-                  width=900, height=600,
-                  labels={"ID": "Número absoluto de pantentes",
-                          "mes_ano_dep": "Data de depósito"
-                          },
-                  template="simple_white")
+def plot_line(counts):
+    fig=px.line(counts,
+    x='mes_ano_dep', y='ID',
+    color='parceria', 
+    markers=True,
+    width=900, height=600,
+    labels={ # replaces default labels by column name
+                "ID": "Número absoluto de pantentes",  
+                "mes_ano_dep": "Data de depósito",
+                "parceria": "Tipo de participação"
+            },
+    color_discrete_map={ # replaces default color mapping by value
+                "Titular": "lightgreen", "Parceiro": "darkgreen"
+            },
+    template="simple_white")
     fig.update_xaxes(tickangle=-90)
     st.write(fig)
 
+def plot_pie(counts):
+    fig=px.pie(counts,
+    values='ID',
+    names='parceria',
+    color='parceria',
+    width=900, height=600,
+    labels={ # replaces default labels by column name
+                "ID": "Número absoluto de pantentes",  
+                "mes_ano_dep": "Data de depósito",
+                "parceria": "Tipo de participação"
+            },
+    color_discrete_map={"Titular": "lightgreen", "Parceiro": "darkgreen"})
+    fig.update_traces(hoverinfo='label+percent', textfont_size=18,
+                  marker=dict(line=dict(color='#000000', width=2)))
+    st.write(fig)
 
 with aba1:
 
-    st.title("Patentes com participação de instituições federais de ensino")
+    st.markdown("#### Série temporal das patentes com participação de instituições federais de ensino")
     data = IFE
     counts = data.loc[data['data_deposito'] >= '2018-01-01'].copy()
-    counts = counts.groupby(['mes_dep', 'ano_dep', 'mes_ano_dep', 'ifes'], observed=True)['ID'].nunique().reset_index()
+    counts = counts.groupby(['mes_dep', 'ano_dep', 'mes_ano_dep', 'parceria'], observed=True)['ID'].nunique().reset_index()
     counts = counts.sort_values(['ano_dep', 'mes_dep']).copy()
-    plot_graph(counts)
+    plot_line(counts)
+
+    st.markdown("#### Distribuição percentual do total de patentes com participação de instituições federais de ensino")
+    counts = data.groupby('parceria', observed=True)['ID'].nunique().reset_index()
+    plot_pie(counts)
+
 
 with aba2:
     
-    st.title("Patentes dcom participação de institutos e centros federais de ensino")
+    st.markdown("#### Série temporal das patentes com participação de institutos e centros federais de ensino")
     data = IF
     counts = data.loc[data['data_deposito'] >= '2018-01-01'].copy()
-    counts = counts.groupby(['mes_dep', 'ano_dep', 'mes_ano_dep', 'ifes'], observed=True)['ID'].nunique().reset_index()
+    counts = counts.groupby(['mes_dep', 'ano_dep', 'mes_ano_dep', 'parceria'], observed=True)['ID'].nunique().reset_index()
     counts = counts.sort_values(['ano_dep', 'mes_dep']).copy()
-    plot_graph(counts)
+    plot_line(counts)
+
+    st.markdown("#### Distribuição percentual do total de patentes com participação de institutos e centros federais de ensino")
+    counts = data.groupby('parceria', observed=True)['ID'].nunique().reset_index()
+    plot_pie(counts)
+
     
 with aba3:   
 
-    st.title("Patentes com participação de universidades federais de ensino")
+    st.markdown("#### Série temporal das patentes com participação de universidades federais de ensino")
     data = UV
     counts = data.loc[data['data_deposito'] >= '2018-01-01'].copy()
-    counts = counts.groupby(['mes_dep', 'ano_dep', 'mes_ano_dep', 'ifes'], observed=True)['ID'].nunique().reset_index()
+    counts = counts.groupby(['mes_dep', 'ano_dep', 'mes_ano_dep', 'parceria'], observed=True)['ID'].nunique().reset_index()
     counts = counts.sort_values(['ano_dep', 'mes_dep']).copy()
-    plot_graph(counts)
+    plot_line(counts)
+
+    st.markdown("#### Distribuição percentual do total de patentes com participação de universidades federais de ensino")
+    counts = data.groupby('parceria', observed=True)['ID'].nunique().reset_index()
+    plot_pie(counts)
+
 
 with aba4:
 
-    st.title('Patentes sem participação de instituições federais de ensino')
+    st.markdown("#### Série temporal das patentes sem participação de instituições federais de ensino")
     data = NIFE
     counts = data.loc[data['data_deposito'] >= '2018-01-01'].copy()
-    counts = counts.groupby(['mes_dep', 'ano_dep', 'mes_ano_dep', 'ifes'], observed=True)['ID'].nunique().reset_index()
+    counts = counts.groupby(['mes_dep', 'ano_dep', 'mes_ano_dep', 'parceria'], observed=True)['ID'].nunique().reset_index()
     counts = counts.sort_values(['ano_dep', 'mes_dep']).copy()
-    plot_graph(counts)
+    plot_line(counts)
+
+    st.markdown("#### Distribuição percentual do total de patentes sem participação de instituições federais de ensino")
+    counts = data.groupby('parceria', observed=True)['ID'].nunique().reset_index()
+    plot_pie(counts)
