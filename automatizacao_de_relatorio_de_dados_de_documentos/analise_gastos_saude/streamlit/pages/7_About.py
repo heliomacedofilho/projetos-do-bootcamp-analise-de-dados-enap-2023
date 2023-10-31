7ad697e7044ba9e76e72a0cec824c0d1ef1547ab
chelacamb <110421496+MichelaCamboim@users.noreply.github.com>
2023-10-31 09:15:26 -0300

    página nova

---
+++ automatizacao_de_relatorio_de_dados_de_documentos/analise_gastos_saude/streamlit/pages/7_About.py
@@ -0,0 +1,92 @@
+import streamlit as st
+
+
+st.set_page_config(page_title="Equipe", page_icon="🏥", layout="wide")
+st.title('_Equipe_ :hospital:')
+st.markdown("---")
+
+st.write("## Projeto Final do Bootcamp de Análise de Dados - ENAP")
+st.write("### Turma Exclusiva para Mulheres - Outubro/2023")
+st.sidebar.success("Select a demo above.")
+
+
+st.markdown(
+    """
+    <p style='text-align: justify;'>
+    Escrever uma introdução para explicar que o trabalho é colaborativo
+    </p>
+    <h5 style='text-align: justify;'>
+    Daiana
+    </h5>
+    <p style='text-align: justify;'>
+    TEXTO SOBRE A DAIANA
+    </p>
+    <p style='text-align: justify;'>
+    [LINK DO GITHUB OU LINKEDIN](url)
+    </p>
+    
+    <h5 style='text-align: justify;'>
+    HARLANE
+    </h5>
+    <p style='text-align: justify;'>
+    TEXTO SOBRE A HARLANE
+    </p>
+    <p style='text-align: justify;'>
+    [LINK DO GITHUB OU LINKEDIN](url)
+    </p>
+
+    <h5 style='text-align: justify;'>
+    Michela Camboim
+    </h5>
+    <p style='text-align: justify;'>
+    Pesquisadora, economista, cientista de dados e desenvolvedora Full Stack Jr do 
+    Núcleo de Estudos Econômicos e Populacionais da Fundação Joaquim Nabuco/Ministério 
+    da Educação. Possuo doutorado em economia pela UFPE, e desenvolvo trabalhos de 
+    análise de dados com uso de estatística descritiva e inferencial, algoritmos de 
+    Machine Learning e uso ferramentas como Python, Linguagem R, DAX, Power BI, 
+    SQL e outras. Como pesquisadora, desenvolvo projetos nas áreas de Economia Social, Economia 
+    da Educação, Economia Regional e Economia do Trabalho, atuando principalmente nos 
+    seguintes temas: educação, avaliação de políticas públicas educacionais, saúde, 
+    desigualdades sociais.
+    </p>
+    <p style='text-align: justify;'>
+    [Github](https://github.com/MichelaCamboim)
+    </p>
+    <p style='text-align: justify;'>
+    [Linkedin](https://www.linkedin.com/in/michela-camboim/)
+    </p>
+
+    <h5 style='text-align: justify;'>
+    PAULA
+    </h5>
+    <p style='text-align: justify;'>
+    TEXTO SOBRE A PAULA
+    </p>
+    <p style='text-align: justify;'>
+    [LINK DO GITHUB OU LINKEDIN](url)
+    </p>
+
+    <h5 style='text-align: justify;'>
+    PRISCILA
+    </h5>
+    <p style='text-align: justify;'>
+    TEXTO SOBRE A PRISCILA
+    </p>
+    <p style='text-align: justify;'>
+    [LINK DO GITHUB OU LINKEDIN](url)
+    </p>
+
+    <h5 style='text-align: justify;'>
+    ROSANA
+    </h5>
+    <p style='text-align: justify;'>
+    TEXTO SOBRE A ROSANA
+    </p>
+    <p style='text-align: justify;'>
+    [LINK DO GITHUB OU LINKEDIN](url)
+    </p>
+
+
+    """,
+    unsafe_allow_html=True
+)
