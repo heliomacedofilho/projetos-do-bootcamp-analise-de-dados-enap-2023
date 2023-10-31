42764582cb32f364ae1c0ae81ea9af9d7363eceb
Priscilla Uchoa Martins <pristark@gmail.com>
2023-10-31 12:05:06 -0300

    Atualizações textuais

---
+++ automatizacao_de_relatorio_de_dados_de_documentos/analise_gastos_saude/streamlit/pages/3_Municípios.py
@@ -0,0 +1,20 @@
+import streamlit as st
+import pandas as pd
+
+st.set_page_config(page_title="Municípios", page_icon="🏥", layout="wide")
+
+st.title('_Análise Descritiva:_ Mapas')
+st.markdown("---")
+
+st.write("## A ideia é trazer mapas com dados dos municípios")
+
+@st.cache_data
+def gerar_df():
+    df = pd.read_excel('./data/variaveis_uf_ano.xlsx',
+                       engine='openpyxl',
+                       usecols='A:F',
+                       nrows=244)
+    return df
+
+df = gerar_df()
+df
