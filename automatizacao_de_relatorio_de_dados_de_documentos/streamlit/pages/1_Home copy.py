f4a4345ed55eda19032d18630ad085428039ef19
Daiana de Paula Sales <8014899+dpsales@users.noreply.github.com>
2023-10-31 18:53:51 -0300

    novo menu e paginas

---
+++ automatizacao_de_relatorio_de_dados_de_documentos/streamlit/pages/1_Home copy.py
@@ -0,0 +1,29 @@
+# pages/page1.py
+import streamlit as st
+
+
+st.set_page_config(
+page_title="Home",
+page_icon="🏥",
+layout='wide',
+initial_sidebar_state='auto')
+
+st.markdown('# Gastos Hospitalares no Brasil')
+st.markdown("---")
+st.write("## Projeto Final do Bootcamp de Análise de Dados - ENAP")
+st.write("### Turma Exclusiva para Mulheres - Outubro/2023")
+st.sidebar.success("Select a page")
+
+
+st.markdown(
+    """
+    Escrever em resumo o objetivo do trabalho
+    ### Fontes dos Dados
+    - Escrever sobre DATASUS
+    - API Portal da Transparência
+    - IBGE - População
+    ### Ferramentas Utilizadas
+    Programa desenvolvido em linguagem Python e com o uso das bibliotecas streamlit, pandas, plotly express
+    
+"""
+    )
